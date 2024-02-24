c0dd5b443cea10365db08cc7b85f99f7c9632a85
SutuSebastian <sebiitv@gmail.com>
2024-02-24 21:07:17 +0200

    init template

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+import forms from "@tailwindcss/forms";
+import flowbite from "flowbite/plugin";
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
+        "node_modules/flowbite-react/lib/esm/**/*.js",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms, flowbite],
+};
